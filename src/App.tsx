5ce9a4b75b9f67d231fbf7f6e6f9a408204e74ba
Marina Medeiros de Araujo Leite <medeiros.marina@academico.ifrn.edu.br>
2022-09-02 17:46:12 -0300

    atividade 2 com comentarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
 import { useState, useEffect } from "react";
 import Button from '@mui/material/Button';
+import { useParams } from "react-router-dom";
+import CommentIcon from '@mui/icons-material/Comment';
+import { Box, CircularProgress, Container, getImageListItemBarUtilityClass, Typography } from "@mui/material";
 
 const App = (props: any) => {
   const [page,setPage] = useState(0);
-  const [userMain,setUserMain] = useState({ id: 0, name: "i" });
+  const [userMain,setUserMain] = useState(
+    { id: 0, name: "i" }
+  );
+  const [postMain,setPostMain] = useState(
+    { id: 1, title: "post", body: "oii" },
+  );
+
+  const PageComentarios = (pros: any) => {
+    const [comentario, setComentario] = useState([
+      { postId: 1, id: 1, name: "comentario1", email: "joao@email.com", body: "oii" },
+      { postId: 2, id: 2, name: "comentario2", email: "jose@email.com", body: "gostei" },
+    ]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postMain.id}`)
+        .then((response) => response.json())
+        .then((json) => {setComentario(json); setLoading(false)});
+    });
+
+    return (
+      <div className="App">
+        <h2>Comentarios do Post de {userMain.name}</h2>
+        <Button onClick={() => {setPage(2)}} variant="text">Voltar para o Post de {userMain.name}</Button>
+        <div className="card">
+          {loading ? <h2>Carregando...</h2> : null}
+          <ul>
+            {comentario.map((comentario) => (
+              <li key={comentario.id}><h3>{comentario.name}</h3><p>{comentario.body}</p> </li>
+              
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 
   const PagePosts = (pros: any) => {
     const [posts, setPosts] = useState([
@@ -11,12 +48,12 @@ const App = (props: any) => {
       { id: 2, title: "post2", body: "abc" },
     ]);
     const [loading, setLoading] = useState(true);
-  
     useEffect(() => {
       fetch("https://jsonplaceholder.typicode.com/users/" + userMain.id + "/posts")
         .then((response) => response.json())
         .then((json) => {setPosts(json); setLoading(false)});
     });
+
     return (
       <div className="App">
         <h2>Posts de {userMain.name}</h2>
@@ -25,7 +62,12 @@ const App = (props: any) => {
           {loading ? <h2>Carregando...</h2> : null}
           <ul>
             {posts.map((post) => (
-              <li key={post.id}><h3>{post.title}</h3><p>{post.body}</p></li>
+              <li key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+                <Button onClick={() => {setPage(3); setPostMain(post)}} variant="text">Comentários do Post {post.id} de {userMain.name}</Button>
+              </li>
+              
               
             ))}
           </ul>
@@ -33,6 +75,7 @@ const App = (props: any) => {
       </div>
     );
   }
+
   const PageTarefas = (props: any) =>{
     const [todos, setTodos] = useState([
       { id: 1, title: "tarefa", completed: true },
@@ -105,20 +148,22 @@ const App = (props: any) => {
     if(page == 2){
       return <PagePosts/>
     }
+    if(page == 3){
+      return <PageComentarios/>
+    }
     else {
       return null;
     } 
   }
+
 	return (
-    <div>
+    <Container>
       <h1>Rede social</h1>
       <Button onClick={() => {setPage(0)}} variant="text">Pagina inicial</Button>
       
-      
       <PageDisplay/>
-    </div>
-    
-  );
+    </Container>
+  )
 };
 
 export default App;
